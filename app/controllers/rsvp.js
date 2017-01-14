53c3920d4da5f7e45c0fd3cba0e5c09c84f62790
dbobbitt <dayton.bobbitt@gmail.com>
2017-01-14 11:35:41 -0500

    RSVP user if they exist as a guest in the database

--- app/controllers/rsvp.js
+++ app/controllers/rsvp.js
@@ -3,7 +3,20 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         pushModel(model) {
-            console.log(`Saving model with address ${model.get('address')}`);
+            this.get('store').query('address', {
+                filter: {
+                    address: model.get('address')
+                }
+            }).then((addresses) => {
+
+                // Set RSVP true only if guest exists in database
+                if (addresses.length > 0) {
+                    model.set('hasRsvp', true);
+                    model.save();
+                }
+            }).catch(() => {
+                console.log('Something went wrong... ¯\_(ツ)_/¯');
+            });
         }
     }
 });
