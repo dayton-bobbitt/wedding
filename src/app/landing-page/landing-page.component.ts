e5094e62f4dfe6ff993ff0f286992d0006924168
dbobbitt <dayton.bobbitt@gmail.com>
2017-05-17 22:29:40 -0400

    Validate event code and update UI if valid

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+  private isAuthorized = false;
 
-  constructor() { }
+  constructor(private http: Http, private router: Router) { }
 
   ngOnInit() {
+    this.isGuestValid().then(() => {
+      this.isAuthorized = true
+    }).catch((error) => console.log('Guest not authorized'));
+  }
+
+  private validateGuest(eventKey: string) {
+    const headers = new Headers();
+    headers.set('eventkey', eventKey);
+
+    this.isGuestValid(headers)
+    .then(() => this.router.navigateByUrl('/our-story'))
+    .catch((err) => alert('Invalid event code'));
+  }
+
+  private isGuestValid(headers?: Headers) {
+    const options = new RequestOptions({ headers, withCredentials: true });
+    const url = 'http://localhost:3000/api/guest/validate';
+    return new Promise((resolve, reject) => {
+      this.http.get(url, options).subscribe(res => resolve(res), err => reject(err));
+    });
   }
 
 }
